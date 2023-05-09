ab6fec66e2e63970efacbbbfc042968f75ab5364
Le Van Duc <vanduc103@gmail.com>
2023-05-09 15:50:41 +0900

    Add backend services

--- web_backend/server.py
+++ web_backend/server.py
@@ -3,11 +3,14 @@
 """Alternative version of the ToDo RESTful server implemented using the
 Flask-RESTful extension."""
 import os
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS
+from flask import Flask, jsonify, abort, make_response, g, request, url_for, session
+from flask_restful import Api, Resource, reqparse, marshal
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from flask_cors import CORS, cross_origin
+from werkzeug.utils import secure_filename
 
 import numpy as np
+import pandas as pd
 
 from database import Database
 from config import upload_folder, allowed_extensions
@@ -48,6 +51,8 @@ initial_templates = [
     'Check null values.',
 ]
 
+def dataset_metadata(data_file):
+    print('aa')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -133,7 +138,7 @@ class CodeGenerationAPI(Resource):
         prompt_id = args['prompt_id']
         prompt_content = "Q:" + args['prompt']
         res = []
-        code = read_code(prompt_list, cell_idx)
+        code = read_code(prompt_list, prompt_id)
         prompt = instruction + prompt_content + ans
             
         #out = response(prompt)
@@ -145,9 +150,9 @@ class CodeGenerationAPI(Resource):
             
         return res
     
-class PromptUpdateAPI(Resource):
+class PromptSaveAPI(Resource):
     def __init__(self):
-        super(PromptUpdateAPI, self).__init__()
+        super(PromptSaveAPI, self).__init__()
         self.reqparse = reqparse.RequestParser()
 
     def post(self):
@@ -164,7 +169,7 @@ api.add_resource(ProgramInitAPI, '/api/v1/init', endpoint='init')
 api.add_resource(PromptInitAPI, '/api/v1/prompt_init', endpoint='prompt_init')
 
 api.add_resource(CodeGenerationAPI, '/api/v1/code_generate', endpoint='code_generate')
-api.add_resource(PromptUpdateAPI, '/api/v1/prompt_update', endpoint='prompt_update')
+api.add_resource(PromptSaveAPI, '/api/v1/prompt_save', endpoint='prompt_save')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=38500)
